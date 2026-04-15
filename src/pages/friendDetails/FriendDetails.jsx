5c055aa1851e3f67a428d9d36e84490a6a1489dc
Iqbal <iqbalhossain6615@gmail.com>
2026-04-16 01:06:32 +0600

    friend details page partially done

--- src/pages/friendDetails/FriendDetails.jsx
+++ src/pages/friendDetails/FriendDetails.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import useFriends from '../../hooks/useFriends';
 
 const FriendDetails = () => {
+    const {id} = useParams();
+    console.log(id,'id');
+    const {friends, loading} = useFriends();
+
+    const expectedFriend = friends.find((friend) => String(friend.id) == id);
+
+    console.log(friends, loading, 'loading', 'friends');
+    console.log(expectedFriend,'expectedFriend');
+    
+    
+    
     return (
         <div>
             Friend Details Page
